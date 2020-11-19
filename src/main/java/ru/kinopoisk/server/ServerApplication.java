2b5eca02721344115fa8ba961fc0959551a75dc2
Artur <artur.sharafutdinov101@yandex.ru>
2020-11-19 17:09:49 +0300

    Add ScheduledNewsService to parse page every 5 min

--- src/main/java/ru/kinopoisk/server/ServerApplication.java
+++ src/main/java/ru/kinopoisk/server/ServerApplication.java
@@ -3,8 +3,9 @@ package ru.kinopoisk.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
 
---
+++ src/main/java/ru/kinopoisk/server/services/ScheduledNewsService.java
@@ -0,0 +1,33 @@
+package ru.kinopoisk.server.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.kinopoisk.server.services.newsDbService.NewsService;
+
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.text.ParseException;
+
+@Component
+public class ScheduledNewsService {
+
+    @Autowired
+    NewsService newsService;
+
+
+    @Scheduled(fixedRate = 10000)
+    public void retakeNews(){
+        boolean isDropped=false;
+        while(!isDropped)
+        try {
+            newsService.parser();
+            isDropped=true;
+            newsService.saveArticleToDb();
+        } catch (IOException e) {
+        } catch (ParseException e) {
+        }
+
+    }
+}
